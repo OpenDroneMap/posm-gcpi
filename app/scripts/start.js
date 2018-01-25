8f9802c5faeafb99203195c5c0a76f4bfd54cf40
Eric Brelsford <ebrelsford@gmail.com>
2018-01-25 16:30:51 -0500

    Poll for filesystem changes in development

--- app/scripts/start.js
+++ app/scripts/start.js
@@ -204,7 +204,8 @@ function runDevServer(host, port, protocol) {
     // Reportedly, this avoids CPU overload on some systems.
     // https://github.com/facebookincubator/create-react-app/issues/293
     watchOptions: {
-      ignored: /node_modules/
+      ignored: /node_modules/,
+      poll: true
     },
     // Enable HTTPS if the HTTPS environment variable is set to 'true'
     https: protocol === "https",
